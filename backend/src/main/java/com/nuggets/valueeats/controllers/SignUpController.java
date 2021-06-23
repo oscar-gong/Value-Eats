174dfebe67b374912356a4fb4be21ee29a54f7f7
58718339 <benjaminmickan@dodo.com.au>
2021-06-23 21:59:47 +1000

    login makes a call to the backend

--- backend/src/main/java/com/nuggets/valueeats/controllers/SignUpController.java
+++ backend/src/main/java/com/nuggets/valueeats/controllers/SignUpController.java
@@ -1,11 +1,11 @@
-package controllers;
+package com.nuggets.valueeats.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 public class SignUpController {
     @RequestMapping(value="/signupDiner", method = RequestMethod.POST)
-    public String signUpDiner() {
+    public void signUpDiner() {
 
     }
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FloatBox } from '../styles/FloatBox';
 import { Title } from '../styles/Title';
 import { AlignCenter } from '../styles/AlignCenter';
@@ -6,6 +6,26 @@ import { Box, TextField, Button } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
 export default function Login () {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        console.log("hello, " + username + " " + password);
+        const loginResult = await fetch("http://localhost:8080/login", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json", 
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        })
+        // const ans = await loginResult.json();
+        console.log(loginResult);
+    }
+
     return (
         <AlignCenter>
             <FloatBox display="flex" flexDirection="column" alignItems="center">
@@ -14,15 +34,16 @@ export default function Login () {
                 </Box>
                 {/* <p>Disrupting the intersection between discount and advertising through centralisation</p> */}
                 <Box pt={1}>
-                    <TextField id="outlined-basic" label="Username" variant="outlined" />
+                    <TextField id="outlined-basic" label="Username" variant="outlined" onChange={e => setUsername(e.target.value)} value={username}/>
                 </Box>
                 <Box pt={2}>
-                    <TextField id="outlined-basic" label="Password" variant="outlined" />
+                    <TextField type="password" id="outlined-basic" label="Password" variant="outlined" onChange={e => setPassword(e.target.value)} value={password}/>
                 </Box>
                 <Box pt={4} pb={4}>
                     <Button variant="contained"
                         color="primary"
-                        endIcon={<SendIcon/>}>
+                        endIcon={<SendIcon/>}
+                        onClick={handleLogin}>
                         Log in
                     </Button>
                 </Box>
