89841200359a651d57abbc93a3ff58bb98d6481e
Ashley Huang <ashleyhuang0201@gmail.com>
2021-06-23 19:10:56 +1000

    onchange register

--- frontend/src/pages/RegisterEatery.jsx
+++ frontend/src/pages/RegisterEatery.jsx
@@ -10,7 +10,14 @@ import SendIcon from '@material-ui/icons/Send';
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 
 export default function RegisterEatery() {
-    const [images, setImages] = React.useState([])
+    const [images, setImages] = React.useState([]);
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [confirmPassword, setConfirmPassword] = React.useState('');
+    const [eateryName, setEateryName] = React.useState('');
+    const [address, setAddress] = React.useState('');
+    const [cuisines, setCuisines] = React.useState('');
+
 
     const handleImages = data => {
         if (data) {
@@ -25,6 +32,17 @@ export default function RegisterEatery() {
         })
     }
 
+    const handleKeyPress = e => {
+        if (e.key === 'Enter') {
+            registerUser();
+        }
+    }
+
+    const registerUser = async() => {
+        // check register details
+        console.log("register")
+    }
+
     return (
         <AlignCenter>
             <FloatBox display="flex" flexDirection="column" alignItems="center">
@@ -32,22 +50,22 @@ export default function RegisterEatery() {
                     <Subtitle>Register Eatery</Subtitle>
                 </Box>
                 <Box width="60%">
-                    <TextField id="outlined-basic" label="Email Address" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" label="Email Address" onChange={e => setEmail(e.target.value)} variant="outlined" fullWidth />
                 </Box>
                 <Box pt={2} width="60%">
-                    <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" label="Password" onChange={e => setPassword(e.target.value)} variant="outlined" fullWidth />
                 </Box>
                 <Box pt={2} width="60%">
-                    <TextField id="outlined-basic" label="Confirm Password" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" label="Confirm Password" onChange={e => setConfirmPassword(e.target.value)} variant="outlined" fullWidth />
                 </Box>
                 <Box pt={2} width="60%">
-                    <TextField id="outlined-basic" label="Eatery Name" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" label="Eatery Name" onChange={e => setEateryName(e.target.value)} variant="outlined" fullWidth />
                 </Box>
                 <Box pt={2} width="60%">
-                    <TextField id="outlined-basic" label="Address" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" label="Address" onChange={e => setAddress(e.target.value)} variant="outlined" fullWidth />
                 </Box>
                 <Box pt={2} width="60%">
-                    <TextField id="outlined-basic" label="Cuisines Offered" variant="outlined" fullWidth />
+                    <TextField id="outlined-basic" label="Cuisines Offered" onChange={e => setCuisines(e.target.value)} variant="outlined" fullWidth />
                 </Box>
                 <Box pt={1}>
                     <Label>
@@ -60,7 +78,8 @@ export default function RegisterEatery() {
                 <Box pt={4} pb={4}>
                     <Button variant="contained"
                         color="primary"
-                        endIcon={<SendIcon/>}>
+                        endIcon={<SendIcon/>}
+                        onClick={registerUser}>
                         Register
                     </Button>
                 </Box>
