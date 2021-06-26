56ef4f3370f1e666027b64f1325b8a842f6b88c9
oscar-gong <oscargong00@gmail.com>
2021-06-26 23:38:26 +1000

    Changed get returns

--- backend/src/main/java/com/nuggets/valueeats/entity/Eatery.java
+++ backend/src/main/java/com/nuggets/valueeats/entity/Eatery.java
@@ -30,23 +30,25 @@ public class Eatery extends User{
         return cuisines;
     }
 
-    public void setCuisines(String cuisines) {
-        String[] splitCuisines = cuisines.split(",");
-        for(int i = 0; i < splitCuisines.length; i++){
-            splitCuisines[i] = splitCuisines[i].trim();
-            splitCuisines[i] = splitCuisines[i].substring(0, 1).toUpperCase() + splitCuisines[i].substring(1).toLowerCase();
-        }
-        this.cuisines = new ArrayList<String>(Arrays.asList(splitCuisines));
+    public void setCuisines(ArrayList<String> cuisines) {
+        // String[] splitCuisines = cuisines.split(",");
+        // for(int i = 0; i < splitCuisines.length; i++){
+        //     splitCuisines[i] = splitCuisines[i].trim();
+        //     splitCuisines[i] = splitCuisines[i].substring(0, 1).toUpperCase() + splitCuisines[i].substring(1).toLowerCase();
+        // }
+        // this.cuisines = new ArrayList<String>(Arrays.asList(splitCuisines));
+        this.cuisines = cuisines;
     }
 
     public ArrayList<String> getMenuPhotos() {
         return menuPhotos;
     }
 
-    public void setMenuPhotos(String menuPhotos) {
-        String[] splitMenuPhotos = menuPhotos.split(",");
-        String[] removeSpaces = Arrays.stream(splitMenuPhotos).map(String::trim).toArray(String[]::new);
-        this.menuPhotos = new ArrayList<String>(Arrays.asList(removeSpaces));
+    public void setMenuPhotos(ArrayList<String> menuPhotos) {
+        // String[] splitMenuPhotos = menuPhotos.split(",");
+        // String[] removeSpaces = Arrays.stream(splitMenuPhotos).map(String::trim).toArray(String[]::new);
+        // this.menuPhotos = new ArrayList<String>(Arrays.asList(removeSpaces));
+        this.menuPhotos = menuPhotos;
     }
 
     @Override
