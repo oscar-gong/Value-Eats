3762e6f9defcd06606d3737f69832a02ed205f21
58718339 <benjaminmickan@dodo.com.au>
2021-06-27 11:56:10 +1000

    register branch merged into login branch

--- backend/src/main/java/com/nuggets/valueeats/controller/AuthenticationController.java
+++ backend/src/main/java/com/nuggets/valueeats/controller/AuthenticationController.java
@@ -12,8 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import java.util.List;
 
 // Allow any requests from anywhere to hit our controller - if we only want localhost hitting our controller - use the commented line underneath
 @CrossOrigin(origins = "*", allowedHeaders = "*")
