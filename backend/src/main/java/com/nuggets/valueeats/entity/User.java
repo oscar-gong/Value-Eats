ab5402baaf8d4286ca0000272cd57afc730b4aab
58718339 <benjaminmickan@dodo.com.au>
2021-06-28 13:30:39 +1000

    removed user getters and setters that lombok handles

--- backend/src/main/java/com/nuggets/valueeats/entity/User.java
+++ backend/src/main/java/com/nuggets/valueeats/entity/User.java
@@ -23,41 +23,4 @@ public class User {
     private String alias;
     private String address;
     private String token;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getAlias() {
-        return alias;
-    }
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public String getToken() {
-        return token;
-    }
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
