df49c1f20267620c73bce74a4e3b90f472f42647
oscar-gong <oscargong00@gmail.com>
2021-07-02 00:26:33 +1000

    Implemented edit review

--- backend/src/main/java/com/nuggets/valueeats/service/DinerFunctionalityService.java
+++ backend/src/main/java/com/nuggets/valueeats/service/DinerFunctionalityService.java
@@ -130,6 +130,67 @@ public class DinerFunctionalityService {
         }
     }
 
+    public ResponseEntity<JSONObject> editReview(String jsonString) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            Review review = objectMapper.readValue(jsonString, Review.class);
+            Diner diner = objectMapper.readValue(jsonString, Diner.class);
+
+            // Check for required inputs
+            if(!(StringUtils.isNotBlank(String.valueOf(diner.getToken())) &&
+                StringUtils.isNotBlank(String.valueOf(review.getEateryId())) &&
+                StringUtils.isNotBlank(String.valueOf(review.getRating())))&&
+                StringUtils.isNotBlank(String.valueOf(review.getId()))
+                ){
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse("Missing fields"));
+            }
+
+            // Check if eatery id exists
+            if(!eateryRepository.existsById(review.getEateryId())){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse("Invalid Eatery ID"));
+            }
+
+            // Check if token is valid
+            if(!dinerRepository.existsByToken(diner.getToken())){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse("Invalid token"));
+            }
+
+            // Check if rating is between 1 to 5 and is in increments of 0.5
+            if(!isValidRating(review.getRating())){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse("Rating must be between 1 and 5 and in 0.5 increments"));
+            }
+
+            // Check if review character length does not exceed 280 characters.
+            if(!isValidMessage(review.getMessage())){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse("Message must not exceed 280 characters"));
+            }
+
+            Long dinerId = dinerRepository.findByToken(diner.getToken()).getId();
+
+            review.setDinerId(dinerId);
+
+            // Check if user already made a review
+            if(reviewRepository.existsByDinerIdAndEateryIdAndReviewId(dinerId, review.getEateryId(), review.getId()) == 0){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse("Review does not exist"));
+            }
+
+
+            reviewRepository.save(review);
+
+            Map<String, Long> dataMedium = new HashMap<>();
+            dataMedium.put("reviewId", review.getId());
+            JSONObject data = new JSONObject(dataMedium);
+            
+            System.out.println(data);
+
+            return ResponseEntity.status(HttpStatus.OK).body(ResponseUtils.createResponse("Review was edited successfully", data));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtils.createResponse(e.toString()));
+        }
+    }
+
     private static boolean isValidRating(Float rating){
         return rating % 0.5 == 0 && rating >= 1 && rating <= 5;
     }
