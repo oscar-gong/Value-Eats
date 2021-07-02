85fa146a9dc09e7f8cfad641ebc86b12be174ccb
oscar-gong <oscargong00@gmail.com>
2021-07-02 23:00:28 +1000

    Removed unused imports

--- frontend/src/pages/EateryProfile.jsx
+++ frontend/src/pages/EateryProfile.jsx
@@ -1,21 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { } from "react";
 import NavBar from "../components/Navbar";
-import StarRating from "../components/StarRating";
 import { MainContainer } from "../styles/MainContainer";
-import Carousel from "react-material-ui-carousel";
-import { useHistory } from "react-router";
 import {
-    Card,
-    Grid,
-    CardContent,
-    CardHeader,
-    CardMedia,
     Typography,
-    makeStyles,
-    Select,
-    MenuItem,
-    InputLabel,
-    Box,
 } from "@material-ui/core";
 
 export default function EateryProfile({ token }) {
