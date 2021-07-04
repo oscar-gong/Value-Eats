b365828703f285251d255e5eef024e00cff25478
58718339 <benjaminmickan@dodo.com.au>
2021-07-04 22:18:44 +1000

    login/register fix

--- frontend/src/styles/AlignCenter.js
+++ frontend/src/styles/AlignCenter.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const AlignCenter = styled.div`
     min-height: 100vh;
+    max-height: 100vh;
+    overflow-y: auto;
     display: flex;
     align-items: center;
     justify-content: center;
