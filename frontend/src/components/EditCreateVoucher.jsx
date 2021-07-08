4cbb350a462898f6f1f4f08385bf2ed9c967af1d
58718339 <benjaminmickan@dodo.com.au>
2021-07-08 21:22:30 +1000

    added date pickers for one off voucher form

--- frontend/src/components/EditCreateVoucher.jsx
+++ frontend/src/components/EditCreateVoucher.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useContext } from 'react';
-import { Dialog, DialogTitle, DialogContent, Box, TextField , DialogActions, Button, Tabs, Tab } from '@material-ui/core';
-import { ProfilePhoto } from '../styles/ProfilePhoto';
-import UploadPhotos from './UploadPhotos';
-import StarRating from './StarRating';
+import { Dialog, DialogTitle, DialogContent, Box, TextField , DialogActions, Button, Tabs, Tab, Radio, RadioGroup, FormControlLabel } from '@material-ui/core';
 import { StoreContext } from '../utils/store';
 import { validRequired } from '../utils/helpers';
+import { KeyboardDatePicker } from '@material-ui/pickers';
 
 export default function EditCreateVoucher ({ voucherId, open, setOpen, isEdit }) {
+  const date = new Date();
 
   const context = useContext(StoreContext);
   const setAlertOptions = context.alert[1];
@@ -15,13 +14,13 @@ export default function EditCreateVoucher ({ voucherId, open, setOpen, isEdit })
     return { value: initialValue, valid: true }
   };
   // isOneOff will either be 0 or 1, used for tabs - 1 for is oneoff, 0 for is not oneoff
-  const [isOneOff, setIsOneOff] = useState(defaultState(0));
-  const [isDineIn, setisDineIn] = useState(defaultState(true));
+  const [isOneOff, setIsOneOff] = useState(0);
+  const [isDineIn, setisDineIn] = useState(defaultState("true"));
   const [discount, setDiscount] = useState(defaultState(0));
   const [quantity, setQuantity] = useState(defaultState(0));
-  const [startTime, setStartTime] = useState(defaultState());
-  const [endTime, setEndTime] = useState(defaultState());
-
+  const [startDateTime, setStartDateTime] = useState(defaultState(date.toISOString().split('T')[0] + "T10:30"));
+  const [endDateTime, setEndDateTime] = useState(defaultState(date.toISOString().split('T')[0] + "T10:30"));
+  console.log(endDateTime);
   const handleUpdateVoucher = async () => {
     console.log("Make the API call here that will udpate this particular Voucher for a particular restaurant");
   }
@@ -74,69 +73,103 @@ export default function EditCreateVoucher ({ voucherId, open, setOpen, isEdit })
           {
             isOneOff === 1 &&
             <Box >
-              <h2>Weekly deal</h2>
             </Box>
           }
           {
             isOneOff === 0 &&
             <Box display="flex" flexDirection="column" alignItems="center">
-              <h2>One off deal</h2>
-              <TextField
-                label="Discount (%)"
-                type="number"
-                onChange={(e) =>
-                  setDiscount({
-                    value: e.target.value,
-                    valid: true
-                  })
-                }
-                allowNegative={false}
-                error={!discount.valid}
-                helperText={
-                  discount.valid ? "" : "Please enter a discount percentage"
-                }
-                onBlur={() => {
-                  validRequired(discount, setDiscount);
-                }}
-                value={discount.value}
-                variant="outlined"
-                fullWidth
+              <Box pt={2}>
+                <RadioGroup row value={isDineIn.value} onChange={(e) => setisDineIn({...isDineIn, value: e.target.value})}>
+                  <FormControlLabel value="true" control={<Radio />} label="Dine in" />
+                  <FormControlLabel value="false" control={<Radio />} label="Takeaway" />
+                </RadioGroup>
+              </Box>
+              <Box py={2}>
+                <TextField
+                  label="Discount (%)"
+                  type="number"
+                  onChange={(e) =>
+                    setDiscount({
+                      value: e.target.value,
+                      valid: true
+                    })
+                  }
+                  allowNegative={false}
+                  error={!discount.valid}
+                  helperText={
+                    discount.valid ? "" : "Please enter a discount percentage"
+                  }
+                  onBlur={() => {
+                    validRequired(discount, setDiscount);
+                  }}
+                  value={discount.value}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Box>
+              <Box py={2}>
+                <TextField
+                  label="Quantity"
+                  type="number"
+                  onChange={(e) =>
+                    setQuantity({
+                      value: e.target.value,
+                      valid: true
+                    })
+                  }
+                  allowNegative={false}
+                  error={!quantity.valid}
+                  helperText={
+                    quantity.valid ? "" : "Please enter the number of vouchers you would like to offer"
+                  }
+                  onBlur={() => {
+                    validRequired(quantity, setQuantity);
+                  }}
+                  value={quantity.value}
+                  variant="outlined"
+                  fullWidth
               />
-              <TextField
-                label="Quantity"
-                type="number"
-                onChange={(e) =>
-                  setQuantity({
-                    value: e.target.value,
-                    valid: true
-                  })
-                }
-                allowNegative={false}
-                error={!quantity.valid}
-                helperText={
-                  quantity.valid ? "" : "Please enter the number of vouchers you would like to offer"
-                }
-                onBlur={() => {
-                  validRequired(quantity, setQuantity);
-                }}
-                value={quantity.value}
-                variant="outlined"
-                fullWidth
-            />
+              </Box>
+              <Box py={2}>
+                <TextField
+                  label="Start at:"
+                  type="datetime-local"
+                  onChange={(e) =>
+                    setStartDateTime({
+                      value: e.target.value,
+                      valid: true
+                    })
+                  }
+                  error={!quantity.valid}
+                  helperText={
+                    quantity.valid ? "" : "Please enter the number of vouchers you would like to offer"
+                  }
+                  value={startDateTime.value}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Box>
+              <Box py={2}>
+                <TextField
+                  label="Start at:"
+                  type="datetime-local"
+                  onChange={(e) =>
+                    setStartDateTime({
+                      value: e.target.value,
+                      valid: true
+                    })
+                  }
+                  error={!quantity.valid}
+                  helperText={
+                    quantity.valid ? "" : "Please enter the number of vouchers you would like to offer"
+                  }
+                  value={startDateTime.value}
+                  variant="outlined"
+                  fullWidth
+                />
+              </Box>
             </Box>
           }
-          {/* <Box pt={2}>
-            <TextField multiline
-                id="outlined-basic"
-                label="Let us know what you think..."
-                onChange={(e) =>
-                  setReviewText(e.target.value)
-                }
-                value={reviewText}
-                variant="outlined"
-                fullWidth
-            />
-          </Box> */}
         </DialogContent>
         <DialogActions>
           {/* Set the things below back to their default states here */}
