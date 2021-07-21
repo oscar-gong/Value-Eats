135f0afd5409ab51e31af0cb14b0c2f865ebfba5
Kevin Chu <51469866+KeenKevin@users.noreply.github.com>
2021-07-21 21:08:16 +1000

    Testing controller loads

--- backend/src/test/java/com/nuggets/valueeats/ValueEatsApplicationTests.java
+++ backend/src/test/java/com/nuggets/valueeats/ValueEatsApplicationTests.java
@@ -5,9 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ValueEatsApplicationTests {
+	@Autowired
+	private HealthController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
