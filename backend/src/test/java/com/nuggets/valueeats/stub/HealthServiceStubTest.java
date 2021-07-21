3a4640c838f3827fb0c5836d71e25c6a7db47be9
Kevin Chu <51469866+KeenKevin@users.noreply.github.com>
2021-07-21 21:08:43 +1000

    Adding sample stub test

---
+++ backend/src/test/java/com/nuggets/valueeats/stub/HealthServiceStubTest.java
@@ -0,0 +1,23 @@
+package com.nuggets.valueeats.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testTest() {
+        assertThat(restTemplate.getForObject("http://localhost:" + port + "/test", String.class)).contains("Test");
+    }
+}
