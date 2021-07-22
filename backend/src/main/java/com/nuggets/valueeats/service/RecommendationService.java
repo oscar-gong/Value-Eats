4249d2245e061fd9dbc81212b7139ed49ce33bc5
oscar-gong <oscargong00@gmail.com>
2021-07-22 20:43:49 +1000

    More eatery details added to search + diner landing

--- backend/src/main/java/com/nuggets/valueeats/service/RecommendationService.java
+++ backend/src/main/java/com/nuggets/valueeats/service/RecommendationService.java
@@ -2,7 +2,15 @@ package com.nuggets.valueeats.service;
 
 import com.nuggets.valueeats.entity.Eatery;
 import com.nuggets.valueeats.repository.EateryRepository;
+import com.nuggets.valueeats.repository.ReviewRepository;
+import com.nuggets.valueeats.repository.voucher.RepeatVoucherRepository;
+import com.nuggets.valueeats.repository.voucher.VoucherRepository;
+import com.nuggets.valueeats.utils.EateryUtils;
+import com.nuggets.valueeats.utils.ResponseUtils;
+
 import me.xdrop.fuzzywuzzy.FuzzySearch;
+
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +18,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
@@ -18,23 +28,32 @@ import java.util.stream.Collectors;
 public class RecommendationService {
     @Autowired
     private EateryRepository eateryRepository;
+    
+    @Autowired
+    private VoucherRepository voucherRepository;
+    
+    @Autowired
+    private RepeatVoucherRepository repeatVoucherRepository;
+    
+    @Autowired
+    private ReviewRepository reviewRepository;
 
-    public ResponseEntity<List<Eatery>> fuzzySearch(final String search) {
+    public ResponseEntity<JSONObject> fuzzySearch(final String search) {
         final PriorityQueue<AbstractMap.SimpleImmutableEntry<Integer, Eatery>> pq = eateryRepository.findAll().stream()
                 .map(a -> new AbstractMap.SimpleImmutableEntry<>(FuzzySearch.weightedRatio(search, a.getCuisines().toString() + "|" + a.getAlias() + "|" + a.getAddress()), a))
                 .collect(Collectors.toCollection(() -> new PriorityQueue<>((a, b) -> b.getKey() - a.getKey())));
 
-        List<Eatery> result = new ArrayList<>(10);
+        List<Object> result = new ArrayList<Object>();
         while (!pq.isEmpty() && result.size() <= 10) {
             Eatery newEatery = pq.poll().getValue();
-
-            newEatery.setEmail(null);
-            newEatery.setPassword(null);
-            newEatery.setReviews(null);
-
-            result.add(newEatery);
+            HashMap<String, Object> eatery = EateryUtils.createEatery(voucherRepository, repeatVoucherRepository, reviewRepository, newEatery);
+            result.add(eatery);
         }
 
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+        Map<String, Object> dataMedium = new HashMap<>();
+        dataMedium.put("eateryList", result);
+        JSONObject data = new JSONObject(dataMedium);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseUtils.createResponse(data));
     }
 }
