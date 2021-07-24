3d35e4a6ec0594f33d54205288d68f177e3ec884
William <williamjy1018@gmail.com>
2021-07-24 13:43:09 +1000

    Rename test file name

--- backend/src/test/java/com/nuggets/valueeats/UserManagementControllerTests.java
+++ backend/src/test/java/com/nuggets/valueeats/UserManagementControllerTests.java
@@ -36,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class UserManagementServiceTests {
+class UserManagementControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
