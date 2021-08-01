c249074672231862de1ff2ca3fac7b1ed97dd9e3
Ashley Huang <ashleyhuang0201@gmail.com>
2021-08-02 02:44:21 +1000

    boxed redeem voucher title

--- frontend/src/pages/RedeemVoucher.jsx
+++ frontend/src/pages/RedeemVoucher.jsx
@@ -11,6 +11,7 @@ import ConfirmModal from "../components/ConfirmModal";
 import Confetti from "react-confetti";
 import { ShakeHead } from "../styles/ShakeHead";
 import request from "../utils/request";
+import { MainContainer } from "../styles/MainContainer";
 
 export default function RedeemVoucher () {
   const context = useContext(StoreContext);
@@ -27,7 +28,7 @@ export default function RedeemVoucher () {
   const getWindowDimensions = () => {
     return {
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
     };
   };
 
@@ -48,16 +49,28 @@ export default function RedeemVoucher () {
 
   const handleRedeem = async () => {
     console.log("This will call the redeem voucher endpoint");
-    const response = await request.post(`eatery/verify/voucher?code=${code.value}`, {}, auth);
+    const response = await request.post(
+      `eatery/verify/voucher?code=${code.value}`,
+      {},
+      auth
+    );
     const responseData = await response.json();
     if (response.status === 200) {
-      setAlertOptions({ showAlert: true, variant: "success", message: responseData.message });
+      setAlertOptions({
+        showAlert: true,
+        variant: "success",
+        message: responseData.message,
+      });
       setOpenRedeemed(true);
     } else if (response.status === 401) {
       logUserOut(setAuth, setIsDiner);
     } else {
       setCode({ ...code, valid: false });
-      setAlertOptions({ showAlert: true, variant: "error", message: responseData.message });
+      setAlertOptions({
+        showAlert: true,
+        variant: "error",
+        message: responseData.message,
+      });
     }
   };
 
@@ -67,50 +80,65 @@ export default function RedeemVoucher () {
 
   return (
     <>
-      <Confetti opacity={openRedeemed ? 1 : 0}
-        width={width}
-        height={height}
-      />
-      <NavBar isDiner={isDiner}/>
-      <MainContent>
-        <Box display="flex" flexDirection="column" justifyContent="space-evenly" alignItems="center" height="90vh">
-          <PageTitle>Redeem Voucher</PageTitle>
-          <Box pt={2} width="50vw" display="flex" flexDirection="column" justifyContent="center">
-            <ShakeHead
-              aria-label="outlined-basic"
-              label="Enter code here to redeem your voucher"
-              animate={ !code.valid ? "shake-head" : ""}
-              onChange={(e) =>
-                setCode({
-                  value: e.target.value,
-                  valid: true
-                })
-              }
-              error={!code.valid}
-              helperText={code.valid ? "" : "Please try again"}
-              value={code.value}
-              variant="outlined"
-              fullWidth
-            />
-            <Box py={2} display="flex" justifyContent="center">
-              <ButtonStyled variant="contained"
-                color="primary"
-                onClick={handleRedeem}
-              >Redeem</ButtonStyled>
+      <Confetti opacity={openRedeemed ? 1 : 0} width={width} height={height} />
+      <NavBar isDiner={isDiner} />
+      <MainContainer>
+        <MainContent>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-evenly"
+            alignItems="center"
+            height="90vh"
+          >
+            <PageTitle>Redeem Voucher</PageTitle>
+            <Box
+              pt={2}
+              width="50vw"
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+            >
+              <ShakeHead
+                aria-label="outlined-basic"
+                label="Enter code here to redeem your voucher"
+                animate={!code.valid ? "shake-head" : ""}
+                onChange={(e) =>
+                  setCode({
+                    value: e.target.value,
+                    valid: true,
+                  })
+                }
+                error={!code.valid}
+                helperText={code.valid ? "" : "Please try again"}
+                value={code.value}
+                variant="outlined"
+                fullWidth
+              />
+              <Box py={2} display="flex" justifyContent="center">
+                <ButtonStyled
+                  variant="contained"
+                  color="primary"
+                  onClick={handleRedeem}
+                >
+                  Redeem
+                </ButtonStyled>
+              </Box>
             </Box>
           </Box>
-        </Box>
-        <Box height="40vh">
-        </Box>
-      </MainContent>
-      <ConfirmModal open={openRedeemed}
+          <Box height="40vh"></Box>
+        </MainContent>
+      </MainContainer>
+      <ConfirmModal
+        open={openRedeemed}
         handleClose={() => setOpenRedeemed(false)}
         title={"Voucher Redeemed!!!"}
         message={"Would you like to redeem another?"}
         confirmText={"Redeem again"}
         handleConfirm={() => setOpenRedeemed(false)}
         denyText={"Back to Vouchers"}
-        handleDeny={() => history.push("/EateryLanding")}></ConfirmModal>
+        handleDeny={() => history.push("/EateryLanding")}
+      ></ConfirmModal>
     </>
   );
 }
